b99bd5326d719230152615abf3df147b8c464398
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-02 22:02:56 +0300

    Setup basic i18next

---
+++ src/i18/i18.ts
@@ -0,0 +1,15 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+	.use(initReactI18next)
+	.use(LanguageDetector)
+	.init({
+		fallbackLng: "en",
+		resources: {
+			en: {
+				translations: {},
+			},
+		},
+	});
