ba102b720e0ef8cd54a73c256c272b59b34285f1
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-04 23:32:35 +0300

    Add a i18next backend

--- src/i18/i18.ts
+++ src/i18/i18.ts
@@ -1,10 +1,12 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import I18NextHttpBackend from "i18next-http-backend";
 
 i18next
 	.use(initReactI18next)
 	.use(LanguageDetector)
+	.use(I18NextHttpBackend)
 	.init({
 		fallbackLng: "en",
 		resources: {
@@ -19,4 +21,6 @@ i18next
 				},
 			},
 		},
+		saveMissing: true,
+		debug: true,
 	});
