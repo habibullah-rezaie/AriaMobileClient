717a5213379b8e86d93727a5bd51165fbccc3308
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-05 09:35:34 +0300

    Style button component acc. to prototype

--- src/components/pages/LoginPage/LoginForm.tsx
+++ src/components/pages/LoginPage/LoginForm.tsx
@@ -89,7 +89,7 @@ function LoginForm() {
 
 				<div>
 					<button
-						className={`w-full flex flex-row items-center justify-center bg-[#16161A] disabled:cursor-not-allowed disabled:bg-opacity-80 active:bg-opacity-90 focus:bg-opacity-90 transition-opacity duration-300 text-center text-lg text-white py-4 rounded-md`}
+						className={`w-full flex flex-row items-center justify-center bg-appBase hover:bg-appBase/80 focus:ring-2 focus:ring-appBase focus:ring-offset-[3px] active:ring-2 active:ring-appBase active:ring-offset-[3px] disabled:cursor-not-allowed disabled:bg-appBase/80 disabled:ring-appBase/80  transition-opacity duration-300 text-center text-lg text-white py-4 rounded-md`}
 						type="submit"
 						disabled={isSubmitting || !isValid}
 					>
