6d1848371b67d83b3ab717c1d0b07c473f9ee0f7
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-11 01:09:28 +0300

    Display the quick sell form

--- src/routes/authenticated/home/HomePage.tsx
+++ src/routes/authenticated/home/HomePage.tsx
@@ -1,14 +1,15 @@
+import UnbilledSellForm from "components/pages/HomePage/home/NewUnbilledSell";
 import TodaysIncomeLineChart from "components/pages/HomePage/home/TodaysIncomeLineChart";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function HomePage() {
+	const { t, i18n } = useTranslation();
 	return (
-		<div className="w-full h-full">
-			<div style={{ height: "50rem" }}>
+		<div className="w-full h-full bg-white">
+			<div className="grid grid-flow-col grid-cols-[auto_1fr] gap-1 mt-[6.08rem] ">
+				<UnbilledSellForm />
 				<TodaysIncomeLineChart />
 			</div>
-			<Outlet />
 		</div>
 	);
 }
