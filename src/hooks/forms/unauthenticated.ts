c8f903375dc5aefd2f6fd5bacf733bb522689002
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-01-11 12:31:54 +0300

    Move login form hook to new file

---
+++ src/hooks/forms/unauthenticated.ts
@@ -0,0 +1,24 @@
+import { useFormik } from "formik";
+import { t } from "i18next";
+import { object as yupObj, string as yupStr } from "yup";
+
+export function useLoginFormik() {
+	const formik = useFormik({
+		initialValues: { email: "", password: "" },
+		initialErrors: {
+			email: t("login-form-email-required"),
+			password: t("login-form-password-required"),
+		},
+		onSubmit: (_, { setSubmitting }) => {
+			setSubmitting(false);
+		},
+		validationSchema: yupObj({
+			email: yupStr()
+				.email(t("login-form-enter-valid-email").toString())
+				.required(t("login-form-email-required").toString()),
+			password: yupStr().required(t("login-form-password-required").toString()),
+		}),
+	});
+
+	return formik;
+}
